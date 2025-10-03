47296d362c30ba7a573b9baca7fdb334a5e95bb3
Colin <seay0006@algonquinlive.com>
2025-10-03 16:04:33 -0400

    add event listener

---
+++ script.js
@@ -0,0 +1,12 @@
+// creating a form for the date and image to be displayed
+
+const form = document.getElementById('date-form');
+const imageinfo = document.getElementById('image-info');
+
+
+// using the event listener - its job is to to the event being submitted
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+
+};
