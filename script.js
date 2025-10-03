54d7b17d68c934252c1a672d853acf823273b223
Colin <seay0006@algonquinlive.com>
2025-10-03 16:12:01 -0400

    updating html reference for js file

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 // creating a form for the date and image to be displayed
 
 const form = document.getElementById('date-form');
-const imageinfo = document.getElementById('image-info');
+const imageinfo = document.getElementById('image-container');
 
 
 // using the event listener - its job is to to the event being submitted
 form.addEventListener('submit', function(event) {
+//preventDefault - stops people from hitting submit and it just refreshing the page
     event.preventDefault();
+    const dateInput = form.elements
 
 
-};
+
+
+}
