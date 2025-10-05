892a59015469e131404c0c1f577f0cef775daacc
Colin <seay0006@algonquinlive.com>
2025-10-05 12:04:18 -0400

    adding the favourites page functionality updated

--- script.js
+++ script.js
@@ -242,7 +242,29 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         // adding the current apods to the favourites tab
-        
+        if (favouriteBtn) {
+            favouriteBtn.addEventListener('click', () => {
+            if (!currentApod) return;
+            let favourites = JSON.parse(localStorage.getItem('favourites')) || [];
 
+            if (!favourites.some(item => item.date === currentApod.date)) {
+                favourites.push(currentApod);
+                localStorage.setItem('favourites', JSON.stringify(favourites));
+                alert('Added to favourites!');
+            } else {
+                alert('Already in favourites!');
+            }
+        });
+    }
+}
+        // adding the favourires page functionality
+    function removeFromFavourites(date) {
+        // lets the local storage know to remove the item being fetched to avoid duplicates
+    let favourites = JSON.parse(localStorage.getItem('favourites')) || [];
+        // this filters the item out of the array, this prevents the item from being seen there after use
+    favourites = favourites.filter(item => item.date !== date);
+        // updates the local storage with the new array
+    localStorage.setItem('favourites', JSON.stringify(favourites));
+    renderFavourites();
+}
 
-    });
